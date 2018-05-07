ec1a5865ce2e627cd9c78b6db107c0b76013047f
Genesist26 <somsakwp8@outlook.com>
2018-05-07 15:58:44 +0700

    add java class

--- app/src/main/java/com/example/genesis/sngbanking/WithdrawActivity.java
+++ app/src/main/java/com/example/genesis/sngbanking/WithdrawActivity.java
@@ -3,13 +3,11 @@ package com.example.genesis.sngbanking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WithdrawActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_withdraw);
     }
 }
